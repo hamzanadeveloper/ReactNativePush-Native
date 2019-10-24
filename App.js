33ee30103a2e894a4e9ce06ea73eaa8f7c345d89
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-10-24 14:49:04 -0400

    Interface for iOS notifications, environment needs to be tested

--- App.js
+++ App.js
@@ -4,139 +4,204 @@
  *
  * @format
  * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, { Component } from 'react';
-import { TextInput, StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
-import PushController from './PushController.js';
-import appConfig from './app.json';
-import PushNotificationIOS from "@react-native-community/push-notification-ios";
-import {YellowBox} from 'react-native';
+import React, {Component} from 'react';
+import {
+    Alert,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    View,
+} from 'react-native';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
-
-YellowBox.ignoreWarnings([
-    'Require cycle:',
-]);
-
-type Props = {};
-const API_URL = "https://fcm.googleapis.com/fcm/send";
-
-export default class App extends Component<Props> {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      senderId: appConfig.senderID
-    };
-
-    this.notif = new PushController(this.onRegister.bind(this), this.onNotif.bind(this));
-  }
-
-  render() {
-    return (
-        <View style={styles.container}>
-          <Text style={styles.title}>React Native Push Notification</Text>
-          <View style={styles.spacer}></View>
-          <TextInput style={styles.textField} value={this.state.registerToken} placeholder="Register token" />
-          <View style={styles.spacer}></View>
-
-          <TouchableOpacity style={styles.button} onPress={() => { this.notif.localNotif() }}><Text>Local Notification (now)</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => { this.notif.checkPermission(this.handlePerm.bind(this)) }}><Text>Check Permission</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => { this.sendRemote() }}><Text>Remote Notification</Text></TouchableOpacity>
-
-          <View style={styles.spacer}></View>
-          <TextInput style={styles.textField} value={this.state.senderId} onChangeText={(e) => {this.setState({ senderId: e })}} placeholder="GCM ID" />
-          <TouchableOpacity style={styles.button} onPress={() => { this.notif.configure(this.onRegister.bind(this), this.onNotif.bind(this), this.state.senderId) }}><Text>Configure Sender ID</Text></TouchableOpacity>
-          {this.state.gcmRegistered && <Text>GCM Configured !</Text>}
-
-          <View style={styles.spacer}></View>
-        </View>
-    );
-  }
-
-  onRegister(token) {
-    Alert.alert("Registered !", JSON.stringify(token));
-    console.log(token);
-    this.setState({ registerToken: token.token, gcmRegistered: true });
-  }
-
-  onNotif(notif) {
-    console.log(notif);
-    Alert.alert(notif.title, notif.message);
-  }
-
-  sendRemote(){
-    if(this.state.registerToken){
-        let body = {
-            "to": this.state.registerToken,
-            "notification":{
-                "title": "Client",
-                "body": "This is a notification!.",
-                "sound": "default",
-                "click_action": "fcm.ACTION.HELLO"
-            },
-            "priority": 10
-        }
-
-        this._send(JSON.stringify(body), "notification");
+class Button extends React.Component<$FlowFixMeProps> {
+    render() {
+        return (
+            <TouchableHighlight
+                underlayColor={'white'}
+                style={styles.button}
+                onPress={this.props.onPress}>
+                <Text style={styles.buttonLabel}>{this.props.label}</Text>
+            </TouchableHighlight>
+        );
     }
-    else{
-        Alert.alert("Configure Sender ID First.")
-        return null
-    }
-  }
-
-  _send(body, type) {
-      let headers = new Headers({
-          "Content-Type": "application/json",
-          "Content-Length": parseInt(body.length),
-          "Authorization": "key=AAAAnBLPaBI:APA91bFW9LU8lZ24h8g4aJ-UaYJkl8ce-eDnv7rPuc6jiisoUvacFQkdHGtmF8NUL2s6acK36QlRKb7PtTIGWJOWboFTf81243KpmpItPbq179NtPbOwfNYPNXk2wCsc5WAzkmZnOVqH"
-      });
-
-      fetch(API_URL, { method: "POST", headers, body })
-          .then(response => console.log("Send " + type + " response", response))
-          .catch(error => console.log("Error sending " + type, error));
-  }
-
-  handlePerm(perms) {
-    Alert.alert("Permissions", JSON.stringify(perms));
-  }
 }
 
+type Props = {};
+type State = {
+    permissions: Object,
+};
+export default class App extends Component<Props, State> {
+    state = {
+        permissions: {},
+    };
+
+    UNSAFE_componentWillMount() {
+        PushNotificationIOS.addEventListener('register', this._onRegistered);
+        PushNotificationIOS.addEventListener(
+            'registrationError',
+            this._onRegistrationError,
+        );
+        PushNotificationIOS.addEventListener(
+            'notification',
+            this._onRemoteNotification,
+        );
+        PushNotificationIOS.addEventListener(
+            'localNotification',
+            this._onLocalNotification,
+        );
+
+        PushNotificationIOS.requestPermissions();
+    }
+
+    componentWillUnmount() {
+        PushNotificationIOS.removeEventListener('register', this._onRegistered);
+        PushNotificationIOS.removeEventListener(
+            'registrationError',
+            this._onRegistrationError,
+        );
+        PushNotificationIOS.removeEventListener(
+            'notification',
+            this._onRemoteNotification,
+        );
+        PushNotificationIOS.removeEventListener(
+            'localNotification',
+            this._onLocalNotification,
+        );
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Button
+                    onPress={this._sendNotification}
+                    label="Send fake notification"
+                />
+
+                <Button
+                    onPress={this._sendLocalNotification}
+                    label="Send fake local notification"
+                />
+                <Button
+                    onPress={() =>
+                        PushNotificationIOS.setApplicationIconBadgeNumber(42)
+                    }
+                    label="Set app's icon badge to 42"
+                />
+                <Button
+                    onPress={() => PushNotificationIOS.setApplicationIconBadgeNumber(0)}
+                    label="Clear app's icon badge"
+                />
+                <View>
+                    <Button
+                        onPress={this._showPermissions.bind(this)}
+                        label="Show enabled permissions"
+                    />
+                    <Text>{JSON.stringify(this.state.permissions)}</Text>
+                </View>
+            </View>
+        );
+    }
+
+    _sendNotification() {
+        require('RCTDeviceEventEmitter').emit('remoteNotificationReceived', {
+            remote: true,
+            aps: {
+                alert: 'Sample notification',
+                badge: '+1',
+                sound: 'default',
+                category: 'REACT_NATIVE',
+                'content-available': 1,
+            },
+        });
+    }
+
+    _sendLocalNotification() {
+        PushNotificationIOS.presentLocalNotification({
+            alertBody: 'Sample local notification',
+            applicationIconBadgeNumber: 1
+        });
+    }
+
+    _onRegistered(deviceToken) {
+        console.log("THE DEVICE TOKEN IS: ", deviceToken)
+        Alert.alert(
+            'Registered For Remote Push',
+            `Device Token: ${deviceToken}`,
+            [
+                {
+                    text: 'Dismiss',
+                    onPress: null,
+                },
+            ],
+        );
+    }
+
+    _onRegistrationError(error) {
+        Alert.alert(
+            'Failed To Register For Remote Push',
+            `Error (${error.code}): ${error.message}`,
+            [
+                {
+                    text: 'Dismiss',
+                    onPress: null,
+                },
+            ],
+        );
+    }
+
+    _onRemoteNotification(notification) {
+        const result = `Message: ${notification.getMessage()};\n
+      badge: ${notification.getBadgeCount()};\n
+      sound: ${notification.getSound()};\n
+      category: ${notification.getCategory()};\n
+      content-available: ${notification.getContentAvailable()}.`;
+
+        Alert.alert('Push Notification Received', result, [
+            {
+                text: 'Dismiss',
+                onPress: null,
+            },
+        ]);
+    }
+
+    _onLocalNotification(notification) {
+        Alert.alert(
+            'Local Notification Received',
+            'Alert message: ' + notification.getMessage(),
+            [
+                {
+                    text: 'Dismiss',
+                    onPress: null,
+                },
+            ],
+        );
+    }
+
+    _showPermissions() {
+        PushNotificationIOS.checkPermissions(permissions => {
+            this.setState({permissions});
+        });
+    }
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  button: {
-    borderWidth: 1,
-    borderColor: "#000000",
-    margin: 5,
-    padding: 5,
-    width: "70%",
-    backgroundColor: "#DDDDDD",
-    borderRadius: 5,
-  },
-  textField: {
-    borderWidth: 1,
-    borderColor: "#AAAAAA",
-    margin: 5,
-    padding: 5,
-    width: "70%"
-  },
-  spacer: {
-    height: 10,
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 20,
-    textAlign: "center",
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    button: {
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonLabel: {
+        color: 'blue',
+    },
 });
